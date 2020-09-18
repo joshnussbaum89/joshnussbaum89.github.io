0e1eb27e5d9dbee31c2fcb226dc6f31b254cae3b
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-09-18 13:34:41 -0400

    troubleshoot back to top button

--- js/main.js
+++ js/main.js
@@ -2,5 +2,31 @@
 /*              JavaScript                       */
 /* ============================================= */
 
+
 // initialize Animate on Scroll library
-AOS.init(); 
+AOS.init();
+
+// function delay() {
+//     setTimeout(function () {
+//         toTopIcon.style.display = "block";
+//     }, 2000);
+// }
+
+// window.addEventListener('scroll', delay());
+
+let scrollPos = 0;
+const toTopIcon = document.querySelector('.footer-sound-icon');
+
+function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY < scrollPos) {
+        // Scrolling UP
+        toTopIcon.classList.toggle('fade')
+    } else {
+        // Scrolling DOWN
+        toTopIcon.classList.toggle('fade')
+    }
+    scrollPos = windowY;
+}
+
+window.addEventListener('scroll', checkPosition);
